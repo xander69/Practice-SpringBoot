26272ec4d51d185f44c500ab322d3534ca0b70d5
Alexander Shakhov <xander.shahov@gmail.com>
2022-02-22 23:28:26 +0300

    added integration tests for login page

---
+++ src/test/java/org/xander/practice/webapp/controller/LoginControllerTest.java
@@ -0,0 +1,57 @@
+package org.xander.practice.webapp.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void anonymousTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    @Sql(
+            value = {"/sql/create-user-before.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(
+            value = {"/sql/create-user-after.sql"},
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void loginTest() throws Exception {
+        mockMvc.perform(formLogin().user("admin").password("admin"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void badCredential() throws Exception {
+        mockMvc.perform(post("/login").param("user", "Alfred"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+}
